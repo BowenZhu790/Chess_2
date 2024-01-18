b8efb5ba02d3e6993d4c52face48197be23b97a1
zbw790 <101329662+BowenZhu790@users.noreply.github.com>
2024-01-18 20:21:34 +0000

    implement stock fish for compete with minimax algorithm

--- chess_algo.py
+++ chess_algo.py
@@ -1,14 +1,16 @@
 # import python-chess library
 import chess
 import chess.engine
+import random
 
-path_to_stockfish = "/Applications/Stockfish.app/Contents/MacOS/Stockfish"
+# path_to_stockfish = "D:\stockfish\stockfish\stockfish-windows-x86-64-avx2.exe"
+# engine = chess.engine.SimpleEngine.popen_uci(path_to_stockfish)
 
-engine = chess.engine.SimpleEngine.popen_uci(path_to_stockfish)
+# engine.configure({"Skill Level": 5})
 
 board = chess.Board()
 def evaluate_board(board):
-    board = chess.Board()
+    # board = chess.Board()
 
     if board.is_checkmate():
         if board.turn:
@@ -16,7 +18,7 @@ def evaluate_board(board):
         else:
             return 9999
     
-    # Calculate estimate utitliy for each type of piece
+    # Calculate estimate utitliy for each type of pieces
     white_pawn = len(board.pieces(chess.PAWN, chess.WHITE))
     white_knight = len(board.pieces(chess.KNIGHT, chess.WHITE))
     white_bishop = len(board.pieces(chess.BISHOP, chess.WHITE))
@@ -38,12 +40,57 @@ def evaluate_board(board):
     # return the estimate value
     return pawn_value + knight_value + bishop_value + rook_value + queen_value
 
+def minimax_algorithm_move(board):
+    if board.legal_moves:
+        return random.choice(list(board.legal_moves))
+        
+    
+    
 
-while not board.is_game_over():
-     result = engine.play(board, chess.engine.Limit(time=0.1))
-     board.push(result.move)
+def stock_fish_move(board, engine):
+    result = engine.play(board, chess.engine.Limit(time=0.1))
+    return result.move
 
-engine.quit()
+def main():
+    board = chess.Board()
+    path_to_stockfish = "D:\stockfish\stockfish\stockfish-windows-x86-64-avx2.exe"
+    engine = chess.engine.SimpleEngine.popen_uci(path_to_stockfish)
+    engine.configure({"Skill Level": 5})
+    i = 0
+    while not board.is_game_over():
+        i += 1
+        if board.turn:
+            move = minimax_algorithm_move(board)
+        else:
+            move = stock_fish_move(board, engine)
+    
+        board.push(move)
+        print(board, "\n")
 
+    if board.is_checkmate():
+        result = "white win" if board.turn == chess.BLACK else "black win"
+    elif board.is_stalemate():
+        result = "stale mate"
+    elif board.is_insufficient_material():
+        result = "insufficient material"
+    elif board.can_claim_draw():
+        result = "和棋（可要求和）"
+    elif board.is_seventyfive_moves() or board.is_fivefold_repetition():
+        result = "seventyfive_moves or fivefold_repetition"
+
+    print("Game end! Results:", result)
+    print("Game round:", i/2)
+    engine.quit()
+
+
+
+# test functions
+# while not board.is_game_over():
+#     result = engine.play(board, chess.engine.Limit(time=0.1))
+#     board.push(result.move)
+#     print(board)
+#     print("\n")
+
+main()
 
     
